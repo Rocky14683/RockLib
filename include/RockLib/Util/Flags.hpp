4d839eb7046f8ad58102ee4c1904efed990b5701
Rocky14683 <rocky10208@gmail.com>
2023-10-18 00:30:27 -0400

    delete trajectory flags
    becuz they can be called in action stage

--- include/RockLib/Util/Flags.hpp
+++ include/RockLib/Util/Flags.hpp
@@ -7,18 +7,14 @@
 #include <cstdint>
 
 namespace RockLib {
+
     using Flags = uint8_t;
 
-    namespace controller::flag {
-        const Flags NONE = 0x00;      // 00000000
-        const Flags REVERSE = 0x01;       // 00000001
-        const Flags CONNECT = 0x02;     // 00000010
-    };
-
-    namespace trajectory::flag{
+    namespace flag{
         const Flags NONE = 0x00; // 00000000
         const Flags WIGGLE = 0x01; // 00000001
         const Flags RELATIVE = 0x02; // 00000010
+        const Flags REVERSE = 0x03; // 00000100
     }
 
 } // RockLib
