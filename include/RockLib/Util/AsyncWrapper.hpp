fe868ef4c261d29bcb3517480c41d31cc52a37c4
Rocky14683 <rocky10208@gmail.com>
2023-10-19 21:28:17 -0400

    finish AsyncWrapper class

--- include/RockLib/Util/AsyncWrapper.hpp
+++ include/RockLib/Util/AsyncWrapper.hpp
@@ -2,10 +2,30 @@
 // Created by Rocky Chen on 2023/10/3.
 //
 #pragma once
+#include "pros/rtos.hpp"
+#include <memory>
 
 namespace RockLib {
 
     class AsyncWrapper {
+    protected:
+        AsyncWrapper() = default;
+        virtual ~AsyncWrapper();
+
+        virtual void run() = 0;
+
+    public:
+        virtual void start(const char* name);
+
+        virtual void pause();
+
+        virtual void resume();
+
+        virtual void stop();
+
+    private:
+        static void loopWrapper(void* args);
+        std::unique_ptr<pros::Task> task = nullptr;
 
     };
 
--- src/RockLib/Util/AsyncWrapper.cpp
+++ src/RockLib/Util/AsyncWrapper.cpp
@@ -5,4 +5,34 @@
 #include "RockLib/Util/AsyncWrapper.hpp"
 
 namespace RockLib {
+
+    AsyncWrapper::~AsyncWrapper() {
+        this->task->remove();
+        this->task.reset(nullptr);
+    }
+
+    void AsyncWrapper::start(const char* name){
+        task = std::move(std::make_unique<pros::Task>(loopWrapper, this, name));
+    }
+
+    void AsyncWrapper::pause(){
+        task->suspend();
+    }
+
+    void AsyncWrapper::resume(){
+        task->resume();
+    }
+
+    void AsyncWrapper::stop(){
+        task->remove();
+        task = nullptr;
+    }
+
+    void AsyncWrapper::loopWrapper(void* args){
+        if(args != nullptr){
+            auto it = static_cast<AsyncWrapper*>(args);
+            it->run();
+        }
+    }
+
 } // RockLib
