7c440d60fefc3e52b417b8e7f448f998e999e05e
Rocky14683 <rocky10208@gmail.com>
2023-11-14 18:18:51 -0500

    fix stupid mistake
    IDK what i was doing, i could have just add a default constructor

--- include/RockLib/Control/Action.hpp
+++ include/RockLib/Control/Action.hpp
@@ -18,9 +18,9 @@ namespace RockLib {
         Action& operator=(const Action&) noexcept = default;
 
         struct MotionConstraints_t{
-            double velocity;
-            double acceleration;
-            double jerk;
+            double velocity = 0 ;
+            double acceleration = 0;
+            double jerk = 0;
         };
 
     private:
@@ -28,21 +28,21 @@ namespace RockLib {
 
         void delay(double sec);
 
-        void turn(double angle, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void turn(double angle, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void turnTo(Point target, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void turnTo(Point target, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void move(double distance, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void move(double distance, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void moveTo(Point target, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void moveTo(Point target, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void moveToArc(Point target, double radius, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void moveToArc(Point target, double radius, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void splineTo(Point target, Point c1, Point c2, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void splineTo(Point target, Point c1, Point c2, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void splineTo(Point target, Point c1, Flags flags = flag::NONE, MotionConstraints_t constraint = {0, 0, 0});
+        void splineTo(Point target, Point c1, Flags flags = flag::NONE, MotionConstraints_t constraint = {});
 
-        void splineTo(std::vector<Point> target, Flags flags = flag::NONE, MotionConstraints_t constraints = {0, 0, 0});
+        void splineTo(std::vector<Point> target, Flags flags = flag::NONE, MotionConstraints_t constraints = {});
     };
 
 } // RockLib
