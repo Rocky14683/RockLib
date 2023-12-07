be2d0c2924e0fd17db9ce97b19f114cbcd8d8781
Rocky14683 <rocky10208@gmail.com>
2023-12-07 11:52:24 -0500

    Create actionContainer and update all instance in constraint class to optional

--- include/RockLib/Control/ActionBuilder.hpp
+++ include/RockLib/Control/ActionBuilder.hpp
@@ -6,6 +6,7 @@
 
 #include <map>
 #include <functional>
+#include <optional>
 #include "RockLib/Control/TrajectoryRunner.hpp"
 #include "RockLib/Util/Flags.hpp"
 #include "RockLib/Util/Point.hpp"
@@ -18,48 +19,63 @@ namespace RockLib {
 
     public:
         struct MotionConstraints_t {
-            double velocity = 0;
-            double acceleration = 0;
-            double jerk = 0;
+            std::optional<double> velocity = std::nullopt;
+            std::optional<double> acceleration = std::nullopt;
+            std::optional<double> jerk = std::nullopt;
+
+            explicit operator bool() const {
+                return this != &const_cast<MotionConstraints_t &>(DEFAULT_CONSTRAINT);
+            }
+
+            const static MotionConstraints_t DEFAULT_CONSTRAINT;
+        };
+
+        struct ActionContainer {
+            std::function<void()> lambda;
+            std::vector<Pose> intendedPath;
+            Flags flags;
+            std::optional<MotionConstraints_t> constraint = std::nullopt;
         };
 
         ActionBuilder(TrajectoryRunner *parent);
 
         ActionBuilder &delay(double msec);
 
-        ActionBuilder &turn(double angle, Flags flags = flag::NONE,
-                            ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &turn(double angle, Flags flags = Flag::NONE,
+                            const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &turnTo(Point target, Flags flags = flag::NONE,
-                              ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &turnTo(Point target, Flags flags = Flag::NONE,
+                              const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &move(double distance, Flags flags = flag::NONE,
-                            ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &move(double distance, Flags flags = Flag::NONE,
+                            const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &moveTo(Point target, Flags flags = flag::NONE,
-                              ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &moveTo(Point target, Flags flags = Flag::NONE,
+                              const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &moveToArc(Point target, double radius, Flags flags = flag::NONE,
-                                 ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &moveToArc(Point target, double radius, Flags flags = Flag::NONE,
+                                 const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &splineTo(Point target, Point c1, Point c2, Flags flags = flag::NONE,
-                                ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &splineTo(Point target, Point c1, Point c2, Flags flags = Flag::NONE,
+                                const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &splineTo(Point target, Point c1, Flags flags = flag::NONE,
-                                ActionBuilder::MotionConstraints_t constraint = {0, 0, 0});
+        ActionBuilder &splineTo(Point target, Point c1, Flags flags = Flag::NONE,
+                                const ActionBuilder::MotionConstraints_t &constraint = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
-        ActionBuilder &splineTo(std::vector<Point> target, Flags flags = flag::NONE,
-                                ActionBuilder::MotionConstraints_t constraints = {0, 0, 0});
+        ActionBuilder &splineTo(const std::initializer_list<Point> &target, Flags flags = Flag::NONE,
+                                const ActionBuilder::MotionConstraints_t &constraints = MotionConstraints_t::DEFAULT_CONSTRAINT);
 
         void run(bool waitUntilSettled = false);
 
     private:
         void reset();
 
+        void setConstraint(const ActionBuilder::MotionConstraints_t &constraint);
+
         TrajectoryRunner *parent;
 
         uint32_t actionID;
-        std::map<uint32_t, void *> holder;
+        std::map<uint32_t, ActionContainer> holder;
     };
 
 } // namespace RockLib
